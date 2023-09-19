8dbfa036ff3ac913edad538dfe4933e25db0ddcb
Marc Hyeler <birdpump@gmail.com>
2023-09-19 19:09:37 +0000

    create dev start script

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,23 +1,31 @@
-import {Sequelize, DataTypes} from "sequelize";
+import { Sequelize } from "sequelize";
 import path from 'path';
 
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-//dev db
-export const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: path.join(__dirname, '../local-db', 'devDb.sqlite'),
-    logging: true,
-});
+const { NODE_ENV } = process.env;
 
-//prod db
-// export const sequelize = new Sequelize({
-//     dialect: 'mysql',
-//     host: '10.1.13.12',
-//     username: 'birdpump',
-//     password: 'ggfdlkgg4',
-//     database: 'test',
-//     logging: false,
-// });
+let sequelize; // Declare sequelize here
+
+if (NODE_ENV === 'production') {
+    // Production database configuration
+    sequelize = new Sequelize({
+        dialect: 'mysql',
+        host: '10.1.13.12',
+        username: 'birdpump',
+        password: 'ggfdlkgg4',
+        database: 'test',
+        logging: false,
+    });
+} else {
+    // Dev database configuration
+    sequelize = new Sequelize({
+        dialect: 'sqlite',
+        storage: path.join(__dirname, '../local-db', 'devDb.sqlite'),
+        logging: true,
+    });
+}
+
+export { sequelize }; // Export sequelize here
