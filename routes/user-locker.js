d9215c53cb5d4000fc94b9267fcf1c0cfe902c11
Marc Hyeler <birdpump@gmail.com>
2023-09-21 21:10:13 +0000

    fix promise

--- routes/user-locker.js
+++ routes/user-locker.js
@@ -5,20 +5,28 @@ import express from 'express';
 export const userlockerRouter = express.Router();
 
 
-userlockerRouter.get('/lookuplocker/:studentId', (req, res) => {
-    // Get the locker number from the request parameters
-    const studentId = req.params.studentId;
-    console.log(studentId)
-    // Assuming you have some database or data source to look up locker information
-    // Replace the following with your actual data retrieval logic
-    const lockerData = getUser(studentId)
-    console.log(lockerData)
-    // Send the locker data as JSON response
-    if (lockerData) {
+userlockerRouter.get('/lookuplocker/:studentId', async (req, res) => {
+    try {
+      // Get the locker number from the request parameters
+      const studentId = req.params.studentId;
+      console.log(studentId);
+  
+      // Assuming you have some database or data source to look up locker information
+      // Replace the following with your actual data retrieval logic
+      const lockerData = await getUser(studentId);
+      console.log(lockerData);
+  
+      // Send the locker data as JSON response
+      if (lockerData) {
         // Send the locker data as JSON response if it exists
         res.json(lockerData);
       } else {
         // Send an error status code (e.g., 404 Not Found) and an error message as JSON response
         res.status(404).json({ error: 'User not found' });
       }
+    } catch (error) {
+      // Handle any errors that occur during the data retrieval or processing
+      res.status(500).json({ error: 'Internal server error' });
+    }
   });
+  
