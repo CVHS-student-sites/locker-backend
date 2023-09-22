5ccbf8f1cb55f11484d62d1ba64cd116a4ee5036
birdpump <birdpump@gmail.com>
2023-09-22 10:58:38 -0700

    fix env again

--- models/locker.js
+++ models/locker.js
@@ -3,22 +3,25 @@ import { Sequelize, DataTypes } from "sequelize";
 import { User } from './user.js';
 
 const Locker = sequelize.define('Locker', {
-  // Define locker attributes (e.g., locker number, location, etc.)
   lockerNumber: {
     type: DataTypes.STRING,
     allowNull: false,
     primaryKey: true,
     unique: true,
   },
+
   location: {
     type: DataTypes.STRING,
     allowNull: false,
   },
+
+  status:{
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  }
   // Add any other locker attributes you need
 });
 
-// Define the association between User and Locker
-// Define the association between User and Locker
 Locker.hasMany(User);
 User.belongsTo(Locker);
 
