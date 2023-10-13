68ffd3be03516fe8c2be240548dd8e6245ba973d
Marc Hyeler <birdpump@gmail.com>
2023-10-13 15:33:15 +0000

    update networks

--- config/sequelize.js
+++ config/sequelize.js
@@ -25,7 +25,7 @@ if (deployType == 'production') {
     // Production database configuration
     sequelize = new Sequelize({
         dialect: 'mysql',
-        host: '10.1.13.11',
+        host: 'mysql-container',
         username: process.env.USERNAME,
         password: process.env.PASSWORD,
         database: 'test',
