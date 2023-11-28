2a55cda02fc569989bf7e47372a06a0560945b16
birdpump <birdpump@gmail.com>
2023-11-27 16:13:43 -0800

    add upload csv functionality

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,10 @@
 import { createUser, createLocker, createUserjoinLocker, getUser, getLocker, joinUsertoLocker } from "../controllers/user-locker.js";
 import { ensureAuthenticated } from "./auth.js";
 
-import express from 'express';
+import {Readable} from "stream";
+import csvParser from "csv-parser";
+import multer from "multer";
+import {LockerData} from "../models/lockerData.js";
 
 export const adminRouter = express.Router();
 
@@ -21,4 +24,66 @@ adminRouter.get('/lookup-user/:studentId', ensureAuthenticated, async (req, res)
   } catch (error) {
     res.status(500).json({ error: 'Internal server error' });
   }
+});
+
+
+//start goofy ah code todo fix this, make it cleaner
+
+const storage = multer.memoryStorage(); // Store the file in memory
+const upload = multer({ storage: storage });
+
+const convertStringValuesToNumbers = (obj) => {
+  const result = {};
+  result.Num = parseInt(obj.Num);
+  result.Location = {
+    Building: parseFloat(obj.Building),
+    TopBottom: obj.TopBottom === 'bottom' ? 0 : 1, //todo fix this
+    Floor: parseFloat(obj.Floor),
+    X: parseFloat(obj.X),
+    Y: parseFloat(obj.Y),
+  };
+  return result;
+};
+
+
+export async function createdataLocker(lockerNumber, location) {
+  try {
+    let locker = await LockerData.create({
+      lockerNumber: lockerNumber,
+      location: location,
+    });
+    return locker;
+  } catch (err) {
+    console.error(err);
+    return false;
+  }
+}
+
+adminRouter.post('/upload', upload.single('csvFile'), async (req, res) => {
+  let final;
+
+  // Access the uploaded file buffer
+  const fileBuffer = req.file.buffer.toString('utf8');
+
+  // Parse the CSV data
+  const parsedData = [];
+  Readable.from(fileBuffer) // Use Readable.from to create a readable stream
+      .pipe(csvParser({
+        columns: true,
+        bom: true,
+      }))
+      .on('data', (row) => {
+        parsedData.push(row);
+      })
+      .on('end', async () => {
+
+        final = parsedData.map(convertStringValuesToNumbers);
+        // res.json({ data: final });
+        for (const record of final) {
+          const lockerNumber = record.Num;
+          const location = record.Location;
+
+          await createdataLocker(lockerNumber, location);
+        }
+      });
 });
---
+++ tools/csvupload.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import multer from 'multer';
+import csvParser from 'csv-parser';
+import { Readable } from 'stream';
+
+const app = express();
+const port = 3000;
+
+// Set up multer to handle file uploads
+const storage = multer.memoryStorage(); // Store the file in memory
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('csvFile'), (req, res) => {
+    // Access the uploaded file buffer
+    const fileBuffer = req.file.buffer.toString('utf8');
+
+    // Parse the CSV data
+    const parsedData = [];
+    Readable.from(fileBuffer) // Use Readable.from to create a readable stream
+        .pipe(csvParser({
+            columns: true,
+            bom: true,
+        }))
+        .on('data', (row) => {
+            parsedData.push(row);
+        })
+        .on('end', () => {
+            // At this point, parsedData contains the parsed CSV rows
+            // You can now do something with the data, such as inserting it into a database
+
+            // For demonstration purposes, let's just send it back as a JSON response
+            res.json({ data: parsedData });
+        });
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening at http://localhost:${port}`);
+});
