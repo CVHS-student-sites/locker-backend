a00f67e63d11c64a57b73f53e4d7069a4b7c5ff3
birdp <birdpump@gmail.com>
2024-01-03 15:29:57 -0800

    add shitty api key auth

--- controllers/admin-data.js
+++ controllers/admin-data.js
@@ -2,8 +2,14 @@ import {createConfig, readConfig, editConfig} from "../utils/config.js";
 
 export async function setGradeRestriction(data){
     try {
-        await editConfig('enabled_grades', JSON.parse(data));
-        console.log(JSON.parse(data));
+        let test = {
+            "grade_9": true,
+            "grade_10": false,
+            "grade_11": false,
+            "grade_12": false
+        }
+        await editConfig('enabled_grades', test);
+        console.log(JSON.parse(test));
     }catch (err){
         return false;
     }
