24cf230c5657ad1e2c4d02057f62c056f84816d2
birdp <birdpump@gmail.com>
2024-01-03 14:20:07 -0800

    add back setupDB

--- config/setupDB.js
+++ config/setupDB.js
@@ -5,13 +5,13 @@ import {createLocker, createUser, joinUsertoLocker} from "../controllers/user-lo
 export async function addTestUsers(){
     await createAdminUser('birdpump','test')
 
-    // await createUser(415633, 'marc test', 'as@stu.gusd.net')
-    //
-    // await createUser(415631, 'marc hyeler', 'ashashas@stu.gusd.net')
-    //
-    // await createLocker('73-13A', {"building": 2, "row": 8})
-    //
-    // await joinUsertoLocker(415633, '73-13A');
-    //
-    // await joinUsertoLocker(415631, '73-13A');
+    await createUser(415633, 'marc test', 'as@stu.gusd.net')
+
+    await createUser(415631, 'marc hyeler', 'ashashas@stu.gusd.net')
+
+    await createLocker('73-13A', {"building": 2, "row": 8})
+
+    await joinUsertoLocker(415633, '73-13A');
+
+    await joinUsertoLocker(415631, '73-13A');
 }
