cec36f331458753f650b63d4936aead05312d0e8
birdp <birdpump@gmail.com>
2024-01-03 15:08:32 -0800

    add json parse

--- controllers/admin-data.js
+++ controllers/admin-data.js
@@ -1,5 +1,5 @@
 import {createConfig, readConfig, editConfig} from "../utils/config.js";
 
 export async function setGradeRestriction(data){
-    await editConfig('enabled_grades', data);
+    await editConfig('enabled_grades', JSON.parse(data));
 }
