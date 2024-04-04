75344b909188d39c48c9726bd0f8b3bcad260f29
birdpump <birdpump@gmail.com>
2024-04-04 12:49:37 -0700

    add task scheduling

---
+++ services/app/scheduleDB.js
@@ -0,0 +1,7 @@
+import schedule from "node-schedule";
+import {verificationQueue} from "../../models/verificationQueue.js";
+
+
+const job = schedule.scheduleJob('*/1 * * * *', function(){
+    console.log('The answer to life, the universe, and everything!');
+});
