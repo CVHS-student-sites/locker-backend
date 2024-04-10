cb3138a1f311e5e30d9e755570fc92c9a678c463
birdpump <birdpump@gmail.com>
2024-04-10 10:37:26 -0700

    change aws region

--- utils/app/sendEmail.js
+++ utils/app/sendEmail.js
@@ -2,7 +2,7 @@ import {SendEmailCommand, SESClient} from "@aws-sdk/client-ses";
 
 // todo keys must be in env
 const sesClient = new SESClient({
-    region: 'us-east-1',
+    region: 'us-west-1',
     credentials: {
         accessKeyId: 'AKIA36WKJYY3L2AFDBEQ',
         secretAccessKey: '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'
