fe4d906707adf8a1197eadb898eaf28f634a75c2
Marc Hyeler <birdpump@gmail.com>
2024-05-22 21:21:24 -0700

    fix logs

--- utils/app/email/sendEmail.js
+++ utils/app/email/sendEmail.js
@@ -44,7 +44,7 @@ export async function sendVerificationEmail(email, link) {
     };
     try {
         let result = await sesClient.send(new SendEmailCommand(params));
-        console.log(`Email Sent: ${email} - Code: ${result.httpStatusCode}`);
+        await console.log(`Email Sent: ${email} - Code: ${result.httpStatusCode}`);
     } catch (err) {
         throw err;
     }
