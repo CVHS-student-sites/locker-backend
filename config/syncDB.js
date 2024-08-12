590944c2a8d37567d4c5ee9034d4e3d209f7e93e
Marc Hyeler <birdpump@gmail.com>
2024-08-12 10:07:58 -0700

    update db sync logic

--- config/syncDB.js
+++ config/syncDB.js
@@ -1,9 +1,21 @@
 import {sequelize} from './sequelize.js';
-
+import {Admin} from "../models/admin.js";
+import {Config} from "../models/config.js";
+import {Locker} from "../models/locker.js";
+import {User} from "../models/user.js";
+import {UserData} from "../models/userData.js";
+import {verificationQueue} from "../models/verificationQueue.js";
 
 async function syncDatabase() {
     try {
         console.log("Starting DB Sync");
+        await Admin.sync();
+        await Config.sync();
+        await Locker.sync();
+        await User.sync();
+        await UserData.sync();
+        await verificationQueue.sync();
+
         await sequelize.sync();
         console.log('Database tables synced successfully');
     } catch (error) {
